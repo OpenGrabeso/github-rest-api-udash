f43f6eca796bcf928a73d50a8ef3f49709cde2be
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2022-11-15 17:21:24 +0100

    Fix: support null for ZonedDateTime

--- rest/ZonedDateTimeCodecs.scala
+++ rest/ZonedDateTimeCodecs.scala
@@ -8,18 +8,18 @@ import com.avsystem.commons.serialization.{GenCodec, GenKeyCodec, Input, Output}
 trait ZonedDateTimeCodecs {
 
   implicit val zonedDateTimeCodec: GenCodec[ZonedDateTime] = new GenCodec[ZonedDateTime] {
-    override def read(input: Input) = {
+    override def read(input: Input): ZonedDateTime = {
       if (input.readNull) null
       else {
         val str = input.readSimple().readString()
         ZonedDateTime.parse(str)
       }
     }
-    override def write(output: Output, value: ZonedDateTime) = {
+    override def write(output: Output, value: ZonedDateTime): Unit = {
       if (value != null) {
         val str = value.toString
         output.writeSimple().writeString(str)
-      }
+      } else output.writeNull()
     }
   }
   implicit val zonedDateTimeKeyCodec: GenKeyCodec[ZonedDateTime] = GenKeyCodec.create(ZonedDateTime.parse,_.toString)
